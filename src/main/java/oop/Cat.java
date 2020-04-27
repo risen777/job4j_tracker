bacb70349f5b9fd30abb4ade52ac787311385499
risen777 <21risen@gmail.com>
2020-04-28 00:42:44 +0300

    1.5. Поля объекта[#267634]

---
+++ src/main/java/oop/Cat.java
@@ -0,0 +1,45 @@
+package oop;
+
+/**
+ * Created by Sergey
+ */
+public class Cat {
+
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+
+    public void giveNick(String nick){
+        this.name = nick;
+
+    }
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Гав");
+        gav.eat("kotleta");
+        gav.show();
+
+        System.out.println();
+
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Черный");
+        black.eat("fish");
+        black.show();
+
+
+
+
+    }
+
+}
