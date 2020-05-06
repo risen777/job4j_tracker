bf9769c684f0533b71d96c4068d26ea743928fce
risen777 <21risen@gmail.com>
2020-05-06 23:44:30 +0300

    0. Что такое инкапсуляция.[#267659]

---
+++ src/main/java/oop/inheritance/Config.java
@@ -0,0 +1,27 @@
+package oop.inheritance;
+
+/**
+ * Created by Sergey
+ */
+public class Config {
+    private String name;
+    private int position;
+    private String[] properties;
+
+    public Config(String name) {
+        this.name = name;
+    }
+
+   public void print() {
+        System.out.println(position);
+    }
+
+    public String getProperty(String key) {
+        return search(key);
+    }
+
+    public String search(String key) {
+        return key;
+    }
+
+}
