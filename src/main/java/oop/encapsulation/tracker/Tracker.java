6a61acb15c94037295d4be34f1ab6c20f50dd98c
risen777 <21risen@gmail.com>
2020-05-13 02:08:57 +0300

    5. Реализовать класс Tracker[#267658]

---
+++ src/main/java/oop/encapsulation/tracker/Tracker.java
@@ -0,0 +1,102 @@
+package oop.encapsulation.tracker;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Random;
+
+/**
+ * Created by Sergey
+ */
+public class Tracker {
+
+    private int ids = 1;
+    private int size = 0;
+    /**
+     * Массив для хранения заявок.
+     */
+    private final Item[] items = new Item[100];
+
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод добавления заявки в хранилище
+     *
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(generateId());
+        items[position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     *
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+
+
+    /**
+     * Поиск по id
+     * @param id
+     * @return найденная item заявка
+     */
+    public Item findById(String id) {
+
+        Item rsl = null;
+        for (int index = 0; index < items.length; index++) {
+            Item item = items[index];
+            if (item.getId().equals(id)) {
+                rsl = item;
+                break;
+            }
+        }
+        return rsl;
+
+    }
+
+
+    /**
+     * Поиск всех элементов массива без null
+     * @return массив без null
+     */
+    public Item[] findAll() {
+        Item[] namesWithoutNull = new Item[items.length];
+        for (int index = 0; index < items.length; index++) {
+            if (items[index] != null) {
+                size++;
+            }
+        }
+        namesWithoutNull = Arrays.copyOf(items, size);
+        return namesWithoutNull;
+    }
+
+
+    /**
+     * Поиск по имени
+     * @param key
+     * @return массив значений найденный по имени
+     */
+    public Item[] findByName(String key) {
+
+        Item[] rsl = new Item[items.length];
+        for (int index = 0; index < items.length; index++) {
+            if (items[index] != null) {
+                if (items[index].getName().equals(key)) {
+                    size++;
+                }
+            }
+        }
+        rsl = Arrays.copyOf(items, size);
+        return rsl;
+    }
+
+}
---
+++ src/test/java/oop/encapsulation/tracker/TrackerTest.java
@@ -0,0 +1,55 @@
+package oop.encapsulation.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sergey
+ */
+public class TrackerTest {
+
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void whenCheckFindAll() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item item1 = new Item("test2");
+        tracker.add(item1);
+        Item[] items = tracker.findAll();
+        assertThat(items[0].getName(), is(item.getName()));
+        assertThat(items[0].getId(), is(item.getId()));
+        assertThat(items[1].getId(), is(item1.getId()));
+        assertThat(items.length,is(2));
+
+    }
+
+    @Test
+    public void whenCheckByName() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item item1 = new Item("test1");
+        tracker.add(item1);
+        Item item2 = new Item("test2");
+        tracker.add(item2);
+        Item[] items = tracker.findByName("test1");
+        for (int i=0;i<items.length;i++){
+           assertThat(items[i].getName(),is("test1"));
+            assertThat(items.length,is(2));
+        }
+
+    }
+
+
+}
