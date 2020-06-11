7bf574ae5dee2b0ddd0bc17e727a92c83f701f6a
risen777 <21risen@gmail.com>
2020-06-12 00:55:16 +0300

    8. Реализация меню за счет шаблона стратегия.[181780#267602]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -36,118 +36,12 @@ public class StartUI {
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(), new ShowAllItemsAction(), new ReplaceAction(), new DeleteAction(), new FindByIdAction(), new FindByNameAction(), new FindByNameAction(), new ExitAction()
+                new CreateAction(), new ShowAllItemsAction(), new ReplaceAction(), new DeleteAction(), new FindByIdAction(), new FindByNameAction(), new ExitAction()
         };
 
         new StartUI().init(input, tracker, actions);
     }
 
-//    public void init(Input input, Tracker tracker) {
-//        boolean run = true;
-//        while (run) {
-//            this.showMenu();
-//            int select = Integer.valueOf(input.askInt("Select: "));
-//            if (select == 0) {
-//                StartUI.createItem(input, tracker);
-//            } else if (select == 1) {
-//                StartUI.showAllItems(tracker);
-//            } else if (select == 2) {
-//                StartUI.replaceItem(input, tracker);
-//
-//            } else if (select == 3) {
-//                StartUI.deleteItem(input, tracker);
-//            } else if (select == 4) {
-//                StartUI.findById(input, tracker);
-//            } else if (select == 5) {
-//                StartUI.findByName(input, tracker);
-//
-//            } else if (select == 6) {
-//                System.out.println("=== Exit Program ====");
-//                run = false;
-//            }
-//        }
-//
-//    }
-//
-//    private void showMenu() {
-//        System.out.println("0. Add new Item");
-//        System.out.println("1. Show all items");
-//        System.out.println("2. Edit item");
-//        System.out.println("3. Delete item");
-//        System.out.println("4. Find item by Id");
-//        System.out.println("5. Find items by name");
-//        System.out.println("6. Exit Program");
-//    }
-//
-//
-//    public static void main(String[] args) {
-//        Input input = new ConsoleInput();
-//        Tracker tracker = new Tracker();
-//        new StartUI().init(input, tracker);
-//    }
-//
-//    public static void createItem(Input input, Tracker tracker) {
-//        System.out.println("=== Create a new Item ====");
-//        String name = input.askStr("Enter name: ");
-//        Item item = new Item(name);
-//        tracker.add(item);
-//    }
-//
-//    public static void showAllItems(Tracker tracker) {
-//        System.out.println("=== Show all items ====");
-//        Item[] items = tracker.findAll();
-//        for (int i = 0; i < items.length; i++) {
-//            System.out.println(items[i]);
-//        }
-//    }
-//
-//    public static void replaceItem(Input input, Tracker tracker) {
-//        System.out.println("=== Edit item ====");
-//        String id = input.askStr("Enter id: ");
-//        String name = input.askStr("Enter name: ");
-//        Item newItem = new Item(name);
-//        if (tracker.replace(id, newItem)) {
-//            System.out.println(newItem);
-//        } else {
-//            System.out.println("Заявка не найдена");
-//
-//        }
-//    }
-//
-//
-//    public static void deleteItem(Input input, Tracker tracker) {
-//        System.out.println("=== Delete item ====");
-//        String id = input.askStr("Enter id: ");
-//        if (tracker.delete(id)) {
-//            System.out.println("Заявка удалена");
-//        } else {
-//            System.out.println("Заявка не найдена");
-//
-//        }
-//    }
-//
-//    public static void findById(Input input, Tracker tracker) {
-//        String id = input.askStr("Enter id: ");
-//        Item item = tracker.findById(id);
-//        if (item == null) {
-//            System.out.println(String.format("Заявка не найдена по id: '%s'", id));
-//        } else {
-//            System.out.println(item);
-//        }
-//    }
-//
-//
-//    public static void findByName(Input input, Tracker tracker) {
-//        String name = input.askStr("Enter name: ");
-//        Item[] items = tracker.findByName(name);
-//        if (items.length > 0) {
-//            for (int i = 0; i < items.length; i++) {
-//                System.out.println(items[i]);
-//            }
-//        } else {
-//            System.out.println(String.format("Заявки не найдены по имени: '%s' ", name));
-//        }
-//    }
 
 }
 
