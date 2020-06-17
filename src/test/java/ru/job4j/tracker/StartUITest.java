f36496e528a3b2ff1677c1cdaddcee83f2648433
risen777 <21risen@gmail.com>
2020-06-18 00:56:20 +0300

    9.2. Тесты вывода на консоль в StartUI[33585#267593]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -18,7 +18,7 @@ public class StartUITest {
     public void whenExit() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"0"}
+                new String[]{"0"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
@@ -26,7 +26,7 @@ public class StartUITest {
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
-                        "Exit" + System.lineSeparator()
+                "Exit" + System.lineSeparator()
         ));
     }
 
@@ -34,16 +34,19 @@ public class StartUITest {
     public void whenCreateItem() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[]{"0", "Item name", "1"}
+                new String[]{"0", "Item name", "1", "Item name", "2"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(out),
-                new ExitAction(out)
+                new FindByNameAction(out),
+                new ExitAction(out),
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
-                "=== Create a new Item ====\r\nExit\r\n"
+                "=== Create a new Item ====" + System.lineSeparator() +
+                        "=== Find items by name ====" + System.lineSeparator() +
+                        "Exit" + System.lineSeparator()
         ));
         assertThat(tracker.findAll()[0].getName(), is("Item name"));
     }
@@ -57,16 +60,19 @@ public class StartUITest {
         /* Входные данные должны содержать ID добавленной заявки item.getId() */
         String replacedName = "New item name";
         Input in = new StubInput(
-                new String[]{"0", item.getId(), replacedName, "1"}
+                new String[]{"0", item.getId(), replacedName, "1",replacedName,"2"}
         );
         UserAction[] actions = {
                 new ReplaceAction(out),
+                new FindByNameAction(out),
                 new ExitAction(out)
         };
         new StartUI(out).init(in, tracker, actions);
         String name = tracker.findById(item.getId()).getName();
         assertThat(out.toString(), is(
-                "=== Edit item ===\r\nExit\r\n"
+                "=== Edit item ===" + System.lineSeparator() +
+                        "=== Find items by name ====" + System.lineSeparator() +
+                        "Exit" + System.lineSeparator()
         ));
         assertThat(name, is(replacedName));
     }
@@ -77,17 +83,21 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         /* Добавим в tracker новую заявку */
         Item item = tracker.add(new Item("Deleted item"));
+        String id = item.getId();
         /* Входные данные должны содержать ID добавленной заявки item.getId() */
         Input in = new StubInput(
-                new String[]{"0", item.getId(), "1"}
+                new String[]{"0", id, "1",id,"2"}
         );
         UserAction[] actions = {
                 new DeleteAction(out),
+                new FindByIdAction(out),
                 new ExitAction(out)
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
-                "=== Delete item ====\r\nExit\r\n"
+                "=== Delete item ====" + System.lineSeparator() +
+                        "=== Find item by Id ====" + System.lineSeparator() +
+                        "Exit" + System.lineSeparator()
         ));
         assertThat(tracker.findById(item.getId()), is(nullValue()));
     }
