1efb689e283bec1cb472a5764fdef8645e6527f9
risen777 <21risen@gmail.com>
2020-08-30 00:56:18 +0300

    2. Очередь с приоритетом на LinkedList [#267574]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -14,31 +14,49 @@ public class PriorityQueue {
      * Метод должен вставлять в нужную позицию элемент.
      * Позиция определяется по полю приоритет.
      * Для вставки использовать add(int index, E value)
+     *
      * @param task задача
      */
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-
+            if (task.getPriority() > element.getPriority()) {
+                this.tasks.remove(element);
+                this.tasks.add(index, task);
+                this.tasks.add(index, element);
+                break;
+            }
+            else if(task.getPriority() < element.getPriority()){
+                this.tasks.add(index, task);
+                break;
+            }
         }
-        this.tasks.add(index, task);
-        Collections.sort(tasks,new TaskComp());
+
+        if (this.tasks.size() == 0) {
+            this.tasks.add(index, task);
+        }
+
+//           else if (task.getPriority() < element.getPriority()) {
+//
+
+
     }
+
 
     public Task take() {
         return tasks.poll();
     }
-    class TaskComp implements Comparator<Task>{
-
-        @Override
-        public int compare(Task o1, Task o2) {
-            if(o1.getPriority() > o2.getPriority()){
-                return 1;
-            } else {
-                return -1;
-            }
-
-        }
-    }
+//    class TaskComp implements Comparator<Task>{
+//
+//        @Override
+//        public int compare(Task o1, Task o2) {
+//            if(o1.getPriority() > o2.getPriority()){
+//                return 1;
+//            } else {
+//                return -1;
+//            }
+//
+//        }
+//    }
 
 }
