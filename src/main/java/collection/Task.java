bc3a4fbbad38c2dc6ae60b09f8e4cd24784a30a4
risen777 <21risen@gmail.com>
2020-10-09 00:59:40 +0300

    1. Уникальные задачи. [#267586]

---
+++ src/main/java/collection/Task.java
@@ -0,0 +1,20 @@
+package collection;
+
+/**
+ * Created by Sergey
+ */
+public class Task {
+
+    private String number;
+    private String description;
+
+    public Task(String number, String description) {
+        this.number = number;
+        this.description = description;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+}
