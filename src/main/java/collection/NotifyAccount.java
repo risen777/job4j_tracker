766e3ece18e51f23a3b1230c208fc3f9854155c5
risen777 <21risen@gmail.com>
2020-10-15 21:35:19 +0300

    3. Модели данных и HashSet. [#267588]

---
+++ src/main/java/collection/NotifyAccount.java
@@ -0,0 +1,24 @@
+package collection;
+
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * Created by Sergey
+ */
+public class NotifyAccount {
+    public static HashSet<Account> sent(List<Account> accounts) {
+        HashSet<Account> rsl = new HashSet<>();
+        // for-each accounts to HashSet;
+        for ( Account account :accounts){
+            rsl.add(account);
+        }
+
+
+        return rsl;
+    }
+
+
+
+
+}
---
+++ src/test/java/collection/NotifyAccountTest.java
@@ -0,0 +1,34 @@
+package collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sergey
+ */
+public class NotifyAccountTest {
+
+
+    @Test
+    public void sent() {
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", "Petr Arsentev", "000001")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f"),
+                        new Account("142", "Petr Arsentev", "000001")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+
+
+}
